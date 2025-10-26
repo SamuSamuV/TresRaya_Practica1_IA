dd51f861d8a843f9c7a067478df2878e7f1080aa
SamuSamuV <samusamu_cb@hotmail.com>
2025-10-26 21:17:37 +0100

    Bases

---
+++ Assets/Scripts/MTD.cs
@@ -0,0 +1,19 @@
+// Copyright © 2025 Samuel Campos Borrego, Laura Gallego Fernández, Icía Fernández Fornos. Todos los derechos reservados.
+
+using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class MTD : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
